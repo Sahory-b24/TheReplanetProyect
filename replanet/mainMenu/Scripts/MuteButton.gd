481a6e80c1e679a1d322fc0a3d2014bb62fa69d0
Laura Villalobos <lvvillalobos@uninorte.edu.co>
2025-05-18 18:25:11 -0500

    sirven botones

--- replanet/mainMenu/Scripts/MuteButton.gd
+++ replanet/mainMenu/Scripts/MuteButton.gd
@@ -16,3 +16,9 @@ func update_icon():
 		texture_normal = ICON_SOUND_OFF
 	else:
 		texture_normal = ICON_SOUND_ON
+
+
+func _on_buttom_back_pressed() -> void:
+	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
+	AudioManager.SFXPlayer.play()
+	SceneTransitions.change_scene_to_file("res://mainMenu/Scenes/main_menu.tscn")
--- replanet/mainMenu/Scripts/options.gd
+++ replanet/mainMenu/Scripts/options.gd
@@ -13,15 +13,6 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_buttom_back_pressed() -> void:
-	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
-	AudioManager.SFXPlayer.play()
-	SceneTransitions.change_scene_to_file("res://mainMenu/Scenes/main_menu.tscn")
-	pass # Replace with function body.
-
-
-
-
 func _on_music_slider_value_changed(value: float) -> void:
 	var db = linear_to_db(value)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("music"), db)
