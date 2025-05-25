bc1b4c0a7491922649f9a7dd8597b3b78bdb753d
isi-25 <idpacheco@uninorte.edu.co>
2025-05-24 22:52:20 -0500

    Added level buttons (scene change)

--- replanet/scenes/world.gd
+++ replanet/scenes/world.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
-func _ready():
-	AudioManager.play_music("res://mainMenu/Assets/Audio/Dark Ambient 1.wav")
+#func _ready():
+	#AudioManager.play_music("res://mainMenu/Assets/Audio/Dark Ambient 1.wav")
+
+func _on_nivel_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	
+func _on_nivel_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Level_2.tscn")
+	
+func _on_nivel_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
