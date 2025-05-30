cc31b696e07862d34fb2ef3216ee428a8e4a6416
isi-25 <idpacheco@uninorte.edu.co>
2025-05-30 08:54:49 -0500

    Added Player_Level2 script

---
+++ replanet/scripts/Global_Mision.gd
@@ -0,0 +1,85 @@
+extends Node
+
+#Collection Body Options (Opciones de Skin Color) 
+var body_fish ={
+	"1" = preload("res://assests/Sprite/fish/char1_fish.png"),
+	"2" = preload("res://assests/Sprite/fish/char2_fish.png"),
+	"3" = preload("res://assests/Sprite/fish/char3_fish.png"),
+	"4" = preload("res://assests/Sprite/fish/char4_fish.png"),
+	"5" = preload("res://assests/Sprite/fish/char5_fish.png"),
+	"6" = preload("res://assests/Sprite/fish/char6_fish.png"),
+	"7"= preload("res://assests/Sprite/fish/char7_fish.png"),
+	"8" = preload("res://assests/Sprite/fish/char8_fish.png")
+}
+#Collection hair options
+var hair_fish ={
+	"none" = null,
+	"1" = preload("res://assests/Sprite/fish/hair/emo_fish.png"),
+	"2" = preload("res://assests/Sprite/fish/hair/ponytail_fish.png"),
+	"3" = preload("res://assests/Sprite/fish/hair/bob_fish.png"),
+	"4" = preload("res://assests/Sprite/fish/hair/braids_fish.png"),
+	"5" = preload("res://assests/Sprite/fish/hair/buzzcut_fish.png"),
+	"7" = preload("res://assests/Sprite/fish/hair/curly_fish.png"),
+	"8" = preload("res://assests/Sprite/fish/hair/extra_long_fish.png"),
+	"9" = preload("res://assests/Sprite/fish/hair/french_curl_fish.png"),
+	"10" = preload("res://assests/Sprite/fish/hair/gentleman_fish.png"),
+	"11" = preload("res://assests/Sprite/fish/hair/long_straight_fish.png"),
+	"12" = preload("res://assests/Sprite/fish/hair/spacebuns_fish.png"),
+	"13" = preload("res://assests/Sprite/fish/hair/wavy_fish.png")
+	
+}
+#Collection shirts options
+var shirts_fish ={
+	"1" = preload("res://assests/Sprite/fish/clothes/basic_fish.png"),
+	"2" = preload("res://assests/Sprite/fish/clothes/floral_fish.png"),
+	"3" = preload("res://assests/Sprite/fish/clothes/dress_fish.png"),
+	"4" = preload("res://assests/Sprite/fish/clothes/overall_fish.png"),
+	"5" = preload("res://assests/Sprite/fish/clothes/sailor_bow_fish.png"),
+	"6" = preload("res://assests/Sprite/fish/clothes/sailor_fish.png"),
+	"7" = preload("res://assests/Sprite/fish/clothes/skull_fish.png"),
+	"8" = preload("res://assests/Sprite/fish/clothes/spaghetti_fish.png"),
+	"9" = preload("res://assests/Sprite/fish/clothes/sporty_fish.png"),
+	"10" = preload("res://assests/Sprite/fish/clothes/stripe_fish.png"),
+	"11" = preload("res://assests/Sprite/fish/clothes/suit_fish.png")
+
+}
+#Collection pants options
+var pants_fish ={
+	"1" = preload("res://assests/Sprite/fish/clothes/pants_fish.png"),
+	"2" = preload("res://assests/Sprite/fish/clothes/skirt_fish.png"),
+	"3" = preload("res://assests/Sprite/fish/clothes/pants_suit_fish.png"),
+	"4" = preload("res://assests/Sprite/fish/clothes/witch_fish.png"),
+	"5" = preload("res://assests/Sprite/fish/clothes/clownGrey_fish.png")
+
+	
+}
+#Collection shoes options
+var shoes_fish ={
+	"1" = preload("res://assests/Sprite/fish/clothes/shoes_fish.png")
+}
+#Collection eyes options
+var eyes_fish ={
+	"1" = preload("res://assests/Sprite/fish/eyes/eyes_fish.png")
+}
+#Collection lips options
+var lips_fish ={
+	"1" = preload("res://assests/Sprite/fish/eyes/lipstick_fish.png")
+}
+#Collection blush options
+var blush_fish ={
+	"none" = null,
+	"1" = preload("res://assests/Sprite/fish/eyes/blush_fish.png")
+}
+#Collection acc options
+var acc_fish ={
+	"none" = null,
+	"1"= preload("res://assests/Sprite/fish/acc/glasses_fish.png"),
+	"2" = preload("res://assests/Sprite/fish/acc/glasses_sun_fish.png"),
+	"3" = preload("res://assests/Sprite/fish/acc/beard_fish.png"),
+	"4" = preload("res://assests/Sprite/fish/acc/earring_emerald_fish.png"),
+	"5" = preload("res://assests/Sprite/fish/acc/earring_red_silver_fish.png"),
+	"6" = preload("res://assests/Sprite/fish/acc/hatGrey_cowboy_fish.png"),
+	"7" = preload("res://assests/Sprite/fish/acc/hat_lucky_fish.png"),
+	"8" = preload("res://assests/Sprite/fish/acc/hat_pumpkin_fish.png"),
+	"9" = preload("res://assests/Sprite/fish/acc/hatGrey_witch_fish.png"),
+}
