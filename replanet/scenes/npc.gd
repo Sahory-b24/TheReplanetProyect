626e1c2084918d017ba4a65ce9d5e30ce4152bd0
Laura_Villalobos <lvvillalobos@uninorte.edu.co>
2025-06-03 12:24:36 -0500

    dialogos listos

--- replanet/scenes/npc.gd
+++ replanet/scenes/npc.gd
@@ -2,30 +2,35 @@ extends Area2D
 class_name NPC
 
 @export var nombre: String = "NPC"
-@export var dialogo_path: Resource  # Asigna diferente .dialogue en cada NPC
-@export var sprite: Texture2D       # Asigna sprite diferente en cada NPC
+@export var dialogo_path: Resource
+@export var sprite: Texture2D
 
-static var npc_activo = null        # Este será el único NPC que responde
+static var npc_activo = null
 
 @onready var sprite_node := $Sprite2D
+@onready var interact_panel := $Panel
 
 func _ready():
 	if sprite:
 		sprite_node.texture = sprite
+	interact_panel.visible = false
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	connect("body_exited", Callable(self, "_on_body_exited"))
 
 func _on_body_entered(body):
-	if body.name == "Player":  
-		NPC.npc_activo = self  # Este NPC se vuelve el activo
+	if body.name == "Player":
+		NPC.npc_activo = self
+		interact_panel.visible = true  # Mostrar panel
 
 func _on_body_exited(body):
 	if body.name == "Player" and NPC.npc_activo == self:
 		NPC.npc_activo = null
+		interact_panel.visible = false  # Ocultar panel
 
 func _process(_delta):
 	if NPC.npc_activo == self and Input.is_action_just_pressed("ui_accept"):
 		mostrar_dialogo()
+		interact_panel.visible = false  # Puedes ocultar el panel al interactuar si quieres
 
 func mostrar_dialogo():
 	if dialogo_path:
