d86e0f66b24396a3a82bf57ab4fa85fb4371a648
Laura_Villalobos <lvvillalobos@uninorte.edu.co>
2025-06-03 04:26:53 -0500

    dialogos

---
+++ replanet/level2/Scenes/npc1.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+@export var nombre: String = "NPC"
+@export var dialogo_path: Resource  # Asigna diferente .dialogue en cada NPC
+@export var sprite: Texture2D       # Asigna sprite diferente en cada NPC
+
+static var npc_activo = null        # Este será el único NPC que responde
+
+@onready var sprite_node := $Sprite2D
+
+func _ready():
+	if sprite:
+		sprite_node.texture = sprite
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
+
+func _on_body_entered(body):
+	if body.name == "Player":  
+		NPC.npc_activo = self  # Este NPC se vuelve el activo
+
+func _on_body_exited(body):
+	if body.name == "Player" and NPC.npc_activo == self:
+		NPC.npc_activo = null
+
+func _process(_delta):
+	if NPC.npc_activo == self and Input.is_action_just_pressed("ui_accept"):
+		mostrar_dialogo()
+
+func mostrar_dialogo():
+	if dialogo_path:
+		DialogueManager.show_example_dialogue_balloon(dialogo_path)
