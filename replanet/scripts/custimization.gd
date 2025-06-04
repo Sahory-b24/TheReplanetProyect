c0854365982b9a8bc08cd446793d51fe04611e07
Laura_Villalobos <lvvillalobos@uninorte.edu.co>
2025-06-03 23:37:17 -0500

    bote

--- replanet/scripts/custimization.gd
+++ replanet/scripts/custimization.gd
@@ -1,17 +1,24 @@
 extends Node2D
 
 @onready var name_box = $Customization_screen/Panel/Name/TextEdit
+@onready var mensaje = $Customization_screen/Panel/Name/MENSAJE
+
 var player_name = ""
 
 func _on_text_edit_text_changed() -> void:
-	player_name = name_box.text
+	player_name = name_box.text.strip_edges()
+	mensaje.visible = false  # Oculta el mensaje mientras escribe
 
 func _on_save_buttom_pressed() -> void:
+	if player_name == "":
+		mensaje.text = "⚠️ Ingresá un nombre antes de continuar"
+		mensaje.visible = true
+		return  # No continúa si no hay nombre
+
 	Global.player_name = player_name
 	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
 	AudioManager.SFXPlayer.play()
 	get_tree().change_scene_to_file("res://InicioLore/scenes/primero.tscn")
-
 
 func _on_random_button_pressed() -> void:
 	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
@@ -32,9 +39,7 @@ func _on_random_button_pressed() -> void:
 		if node != null and node.has_method("randomize"):
 			node.randomize()
 
-
 func _on_buttom_back_pressed() -> void:
 	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
 	AudioManager.SFXPlayer.play()
 	SceneTransitions.change_scene_to_file("res://mainMenu/Scenes/main_menu.tscn")
-	pass # Replace with function body.
