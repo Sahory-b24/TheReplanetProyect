5777d637a769d9f91ad3abbb8211564cdbdf7b99
Valerie <avangulo@uninorte.edu.co>
2025-06-04 17:13:00 -0500

    PARA LOS GAMEPLAYS

--- replanet/scripts/GlobalWorld.gd
+++ replanet/scripts/GlobalWorld.gd
@@ -4,3 +4,8 @@ var modo_juego = ""
 var nivel_1_jugado := false
 var nivel_2_jugado := false
 var nivel_3_jugado := false
+
+func reiniciar_niveles():
+	nivel_1_jugado = false
+	nivel_2_jugado = false
+	nivel_3_jugado = false
