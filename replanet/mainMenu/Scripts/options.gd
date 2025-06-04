c169d262049e7fe34fb71522b2b445ebbd8bca7a
Danna <galavisd@uninorte.edu.co>
2025-06-04 11:47:23 -0500

    FullScreen Button working

--- replanet/mainMenu/Scripts/options.gd
+++ replanet/mainMenu/Scripts/options.gd
@@ -1,38 +1,32 @@
 extends Control
 
-@onready var fullscreen_checkbox = $PantallaCompleta # Cambia
+@onready var fullscreen_btn = $PantallaCompleta
+var is_fullscreen := false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$muisc_bar/Control/VBoxContainer/VolumeBar/musicSlider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("music")))
 	$Sound_bar/Control/VBoxContainer/VolumeBar/sfxSlider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("sfx")))
-	# Al iniciar, ajusta el estado del checkbox según el modo actual de ventana
-	fullscreen_checkbox.button_pressed = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Establecer el modo de pantalla
+	# Sincroniza el botón con la configuración guardada
+	is_fullscreen = Settings.fullscreen_enabled
+	_update_fullscreen_btn()
+	if is_fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 func _on_buttom_back_pressed() -> void:
 	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
 	AudioManager.SFXPlayer.play()
 	SceneTransitions.change_scene_to_file("res://mainMenu/Scenes/main_menu.tscn")
-	pass
-
 
 func _on_music_slider_value_changed(value: float) -> void:
 	var db = linear_to_db(value)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("music"), db)
-	pass # Replace with function body.
-
 
 func _on_sfx_slider_value_changed(value: float) -> void:
 	var db = linear_to_db(value)
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("sfx"), linear_to_db(value))
-	pass # Replace with function body.
-
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("sfx"), db)
 
 func _on_check_button_pressed() -> void:
 	var pressed = $CheckButton.button_pressed
@@ -41,22 +35,29 @@ func _on_check_button_pressed() -> void:
 	else:
 		Overlay.set_mode(0)  # Desactivar filtro
 
-
-
-
 func _on_option_button_item_selected(index: int) -> void:
 	Overlay.set_mode(index)
-	
 
 func _on_exit_world_button_pressed() -> void:
 	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
 	AudioManager.SFXPlayer.play()
-	
 	SceneTransitions.change_scene_to_file("res://mainMenu/Scenes/main_menu.tscn")
 
 
-func _on_pantalla_completa_toggled(toggled_on: bool) -> void:
-	if toggled_on:
+
+func _on_pantalla_completa_pressed() -> void:
+	is_fullscreen = !is_fullscreen
+	Settings.fullscreen_enabled = is_fullscreen
+	_update_fullscreen_btn()
+	if is_fullscreen:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	pass # Replace with function body.
+
+
+func _update_fullscreen_btn():
+	if is_fullscreen:
+		fullscreen_btn.texture_normal = preload("res://mainMenu/Assets/Buttoms/dalto_icon_ckecked.tres")
+	else:
+		fullscreen_btn.texture_normal = preload("res://mainMenu/Assets/Buttoms/dalto_icon.tres")
